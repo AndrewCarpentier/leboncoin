20589aed0e4803eb88b970e3750b9a5159ff35e4
AndrewCarpentier <andr3wcarpentier@gmail.com>
2019-04-08 12:13:32 +0200

    test3

--- src/app/annnonce.service.ts
+++ src/app/annnonce.service.ts
@@ -3,5 +3,29 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AnnnonceService {
 
-  constructor() { }
+  annonces = [{id:'20',titre:'title', description: 'dzeoijdzedazedazed', photo: 'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F1133906920%2F960x0.jpg%3Ffit%3Dscale',prix:300}];;
+  annonceSubject;
+
+  constructor() {
+      this.emit();
+   }
+
+  emit = () => {
+    this.annonceSubject.next(this.annonces);
+  };
+
+  add = (annonce) => {
+    this.annonces.push(annonce);
+    this.emit();
+  };
+
+  search = (value) => {
+    let list = [];
+    this.annonces.forEach((annonce) => {
+      if (annonce.titre.includes(value)) {
+        list.push(annonce);
+      }
+    });
+    return list;
+  };
 }
